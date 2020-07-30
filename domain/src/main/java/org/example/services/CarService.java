9d21a6d493709ee8dbfb3383e68f121976e19610
Tadas Baublys <taduks@gmail.com>
2020-07-30 23:39:30 +0300

    New module "domain" with Spring @Service

---
+++ domain/src/main/java/org/example/services/CarService.java
@@ -0,0 +1,21 @@
+package org.example.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarService {
+
+    public List<String> getCars() {
+        List<String> cars = new ArrayList<>();
+
+        cars.add(0, "Santro");
+        cars.add(1, "Zen");
+        cars.add(2, "Alto");
+        cars.add(3, "Qualis");
+        cars.add(4, "Innova");
+        return cars;
+    }
+}
--- web/src/main/java/org/example/CarBean.java
+++ web/src/main/java/org/example/CarBean.java
@@ -1,27 +1,22 @@
 package org.example;
 
-import javax.faces.bean.ManagedBean;
+import org.example.services.CarService;
+
 import javax.faces.bean.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Named
 @SessionScoped
 public class CarBean {
 
+    @Inject
+    CarService carService;
+
     public List<String> getCarDetails() {
-
-        List<String> cars = new ArrayList<>();
-
-        cars.add(0, "Santro");
-        cars.add(1, "Zen");
-        cars.add(2, "Alto");
-        cars.add(3, "Qualis");
-        cars.add(4, "Innova");
-
-        return cars;
+        return carService.getCars();
     }
 
     public Integer getCarCount() {
